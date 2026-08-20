8716c78fd859cc6d01862fd6eacfa2efd0631af5
ВашеИмя <your_email@example.com>
2026-08-20 14:26:26 +0300

    Сделал метод по выводу вакансий.

--- src/db_manager.py
+++ src/db_manager.py
@@ -2,19 +2,19 @@ import psycopg2
 from dotenv import load_dotenv
 import os
 
-from src.api_client import APICLIENT
+from src.api_client import ApiClient
 
 
-class DBMANAGER:
+class DbManager:
 
     def __init__(self, list_vacanccies=None):
         if list_vacanccies == None:
-            list_vacanccies = APICLIENT.get_response_data('hh_vacancies.json')
+            list_vacanccies = ApiClient.get_response_data('hh_vacancies.json')
         else:
             self.list_vacanccies=list_vacanccies
 
-
-    def get_companies_and_vacancies_count(self):
+    @classmethod
+    def get_companies_and_vacancies_count(cls, db_name):
         '''Получает список всех компаний и количество вакансий у каждой компании.'''
 
         # Устанавливаем связь с файлом .env для получения скрытых данных.
@@ -22,7 +22,7 @@ class DBMANAGER:
         db_password = os.getenv('db_password')
 
         # Устанавливаем соединение с использованием параметров, после чего устанавливаем курсор.
-        conn = psycopg2.connect(host='localhost', port='5432', user='postgres', password=db_password, dbname='coursework_3')
+        conn = psycopg2.connect(host='localhost', port='5432', user='postgres', password=db_password, dbname=db_name)
         cur = conn.cursor()
 
         # Демонстрируем сведения по вакансиям в каждой компании.
@@ -36,56 +36,27 @@ class DBMANAGER:
         # Оставил возвращение на всякий случай для возможного использования.
         return result
 
-    def get_all_vacancies(self):
+    @classmethod
+    def get_all_vacancies(cls, db_name):
         '''Получает список всех вакансий с указанием названия компании,
-        названия вакансии и зарплаты и ссылки на вакансию.'''
-
-        list_result = []
-
-        for vacancy in self.list_vacanccies:
-            company_name = vacancy.get('employer', {}).get('name', {})
-            vacancy_name = vacancy.get('name', {})
-            salary_from = vacancy.get('salary', {}).get('from', {})
-            salary_to = vacancy.get('salary', {}).get('to', {})
-            url_vacancy = vacancy.get('alternate_url', {})
-
-            dict_items = {}
-            dict_items['company_name'] = company_name
-            dict_items['vacancy_name'] = vacancy_name
-            dict_items['salary_from'] = salary_from
-            dict_items['salary_to'] = salary_to
-            dict_items['url_vacancy'] = url_vacancy
-
-            list_result.append(dict_items)
+        названия вакансии, зарплаты и ссылки на вакансию.'''
 
         load_dotenv()
         password = os.getenv('db_password')
 
         conn = psycopg2.connect(user='postgres', password=password, port='5432',
-                                dbname='coursework_3', host='localhost')
+                                dbname=db_name, host='localhost')
         cur = conn.cursor()
         cur.execute('''
-        CREATE TABLE IF NOT EXISTS start_vacancy (
-            company_id SERIAL PRIMARY KEY,
-            company_name VARCHAR(100) NOT NULL,
-            vacancy_name VARCHAR(100) NOT NULL,
-            salary_from VARCHAR(100) NOT NULL,
-            salary_to VARCHAR(100) NOT NULL,
-            url_vacancy VARCHAR(350) NOT NULL
-        )''')
+        SELECT  company.company_name, vacancy_name, salary_from, salary_to, url_vacancy FROM list_vacancy
+        INNER JOIN company ON company.company_id=list_vacancy.company_id
+        ''')
+        result = cur.fetchall()
 
-
-        for vacancy in list_result:
-            cur.execute('''INSERT INTO start_vacancy (company_name, vacancy_name, salary_from, salary_to, url_vacancy) VALUES (%s, %s, %s, %s, %s)''',
-                        (vacancy['company_name'],
-                        vacancy['vacancy_name'],
-                        vacancy['salary_from'],
-                        vacancy['salary_to'],
-                        vacancy['url_vacancy']))
         conn.commit()
         cur.close()
         conn.close()
-        return list_result
+        return result
 
 
 
@@ -96,12 +67,15 @@ class DBMANAGER:
 
 
 
-# obj_1 = DBMANAGER(APICLIENT.get_response_data('hh_vacancies.json'))
-# result = obj_1.get_companies_and_vacancies_count()
+
+# result = DbManager.get_companies_and_vacancies_count('coursework_3')
 # print(result)
 
-
-obj_1 = DBMANAGER(APICLIENT.get_response_data('hh_vacancies.json'))
-result = obj_1.get_all_vacancies()
-for i in result:
+result_2 = DbManager.get_all_vacancies('coursework_3')
+for i in result_2:
     print(i)
+#
+# obj_1 = DBMANAGER(APICLIENT.get_response_data('hh_vacancies.json'))
+# result = obj_1.get_all_vacancies()
+# for i in result:
+#     print(i)
